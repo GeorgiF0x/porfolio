b735333c7f55180105eb169553761a1926cf7889
GeorgiF0x <ge0rgid3v@gmail.com>
2025-04-10 03:16:41 +0200

    añadir componentes FlowBite

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      './src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}',
+    ],
+    theme: {
+      extend: {},
+    },
+    darkMode: 'class', // Puedes usar 'media' si prefieres el modo oscuro del sistema
+    plugins: [],
+  };
+  
